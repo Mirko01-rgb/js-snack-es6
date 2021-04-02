5739c1f99fbe1825e30d0bc450e03efb065d8fe8
Mirko <mirkosbaglia2001@gmail.com>
2021-04-02 16:39:56 +0200

    Esercizio Opzionale utilizzando FILTER

--- js/script.js
+++ js/script.js
@@ -54,74 +54,93 @@
 // Nome sarà l'unica proprietà da compilare,
 // le altre saranno tutte settate a 0.
 
-function es1(){
+// function es1(){
+//
+//   let teamFootball =
+//   [
+//     {
+//       'nome': 'Roma',
+//       'puntiFatti': 0,
+//       'falliSubiti':0
+//     },
+//     {
+//       'nome': 'Lazio',
+//       'puntiFatti': 0,
+//        'falliSubiti':0
+//     },
+//     {
+//       'nome': 'Fiorentina',
+//       'puntiFatti': 0,
+//        'falliSubiti':0
+//     },
+//     {
+//       'nome': 'Sassuolo',
+//       'puntiFatti': 0,
+//        'falliSubiti':0
+//     }
+//   ]
+//     // console.log(teamFootball);
+//   // console.log(teamFootball[0]['nome']);  per entrare nello specifico singolo, poichè non ho un for e quindi non posso usare la teamFootball[i];
+//
+//   for (let i = 0; i < teamFootball.length; i++) {
+//     // console.log(teamFootball[i]['puntiFatti']);
+//     // console.log(teamFootball[i]['falliSubiti']);
+//    const accessArray  = teamFootball[i];
+//    accessArray['puntiFatti'] = getRandom(1, 100);
+//    accessArray['falliSubiti'] = getRandom(1, 100);
+//  }
+//
+//
+//
+//
+//  // Usando la destrutturazione creiamo un nuovo array
+//  // i cui elementi contengono solo nomi e falli subiti
+//  // e stampiamo tutto in console.
+//  const array = [];
+//  for (let i=0;i<teamFootball.length;i++) {
+//    const accessArray  = teamFootball[i];
+//    let { puntiFatti, falliSubiti  } = accessArray;
+//    let resObj = { puntiFatti, falliSubiti};
+//    array.push(resObj);
+//  }
+//   console.log(teamFootball);
+//   console.log(array);
+//
+// }
+//
+// // Generare numeri random al posto degli 0 nelle proprietà:
+// // punti fatti e falli subiti
+// function getRandom(min, max) {
+//   let numMin = min;
+//   let numMax = max;
+//   let rnd = Math.floor(Math.random()* numMax) + numMin;
+//   // console.log(rnd);
+//   return rnd;
+// }
+//
+//
+//
+// function init(){
+//  es1();
+//
+// }
+// $(document).ready(init);
 
-  let teamFootball =
-  [
-    {
-      'nome': 'Roma',
-      'puntiFatti': 0,
-      'falliSubiti':0
-    },
-    {
-      'nome': 'Lazio',
-      'puntiFatti': 0,
-       'falliSubiti':0
-    },
-    {
-      'nome': 'Fiorentina',
-      'puntiFatti': 0,
-       'falliSubiti':0
-    },
-    {
-      'nome': 'Sassuolo',
-      'puntiFatti': 0,
-       'falliSubiti':0
-    }
-  ]
-    // console.log(teamFootball);
-  // console.log(teamFootball[0]['nome']);  per entrare nello specifico singolo, poichè non ho un for e quindi non posso usare la teamFootball[i];
-
-  for (let i = 0; i < teamFootball.length; i++) {
-    // console.log(teamFootball[i]['puntiFatti']);
-    // console.log(teamFootball[i]['falliSubiti']);
-   const accessArray  = teamFootball[i];
-   accessArray['puntiFatti'] = getRandom(1, 100);
-   accessArray['falliSubiti'] = getRandom(1, 100);
- }
 
 
 
 
- // Usando la destrutturazione creiamo un nuovo array
- // i cui elementi contengono solo nomi e falli subiti
- // e stampiamo tutto in console.
- const array = [];
- for (let i=0;i<teamFootball.length;i++) {
-   const accessArray  = teamFootball[i];
-   let { puntiFatti, falliSubiti  } = accessArray;
-   let resObj = { puntiFatti, falliSubiti};
-   array.push(resObj);
- }
-  console.log(teamFootball);
-  console.log(array);
+// Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
+// La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
+// Usiamo i nuovi metodi degli array foreach o filter.
 
+function myFunction(){
+  const arr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+  var indMin = arr[3];
+  var indMax = arr[7];
+  // console.log(arr);
+  const newArray = arr.filter(item => item >= indMin && item <= indMax );
+  console.log(newArray);
 }
 
-// Generare numeri random al posto degli 0 nelle proprietà:
-// punti fatti e falli subiti
-function getRandom(min, max) {
-  let numMin = min;
-  let numMax = max;
-  let rnd = Math.floor(Math.random()* numMax) + numMin;
-  // console.log(rnd);
-  return rnd;
-}
-
-
-
-function init(){
- es1();
-
-}
-$(document).ready(init);
+$(myFunction);
